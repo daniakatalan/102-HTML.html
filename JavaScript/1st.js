dca207ded501ff7aad8dd6019945e2d37f30ae3a
Daniaka1 <>
2021-05-29 22:30:32 +0000

    wrote the first JS code

---
+++ JavaScript/1st.js
@@ -0,0 +1,11 @@
+var userName = prompt("what's you're name?")
+alert("welcome to our website "+userName)
+
+document.write("<P>"+userName+"</P>")
+
+
+var userAnswer = prompt("Do you like traveling?");
+if(userAnswer === "yes"){
+  alert("Enjoy visiting our website")
+}else{alert("Think about it again!")}  
+
