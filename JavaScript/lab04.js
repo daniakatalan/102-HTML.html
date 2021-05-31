f3d4939246f5510451ee3f258172b1dcd6207d9b
Daniaka1 <>
2021-05-31 13:36:48 +0000

    added lab 01

--- JavaScript/lab04.js
+++ JavaScript/lab04.js
@@ -9,3 +9,35 @@ if(userAnswer === "yes"){
   alert("Enjoy visiting our website")
 }else{alert("Think about it again!")}  
 
+
+
+//var userA = prompt("where do you live?")
+
+//for (i = 0; i == ['jordan'], ['syria'], ['lebanon']; i++){
+//alert("Great!")
+//}
+//break; 
+
+
+
+
+let userA1 = prompt("what is your country?")
+let userA2 = prompt("what is your city?")
+
+function printFullA (userA1, userA2){
+  alert("it's grate living in "+userA1 +" "+ userA2);
+}
+
+function sayhi(){
+  document.write("<h1>Hello from function</h1>");
+}
+
+printFullA(userA1, userA2);
+
+sayhi();
+
+
+
+
+
+
